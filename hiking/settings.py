80f3cb8edb585f15b833246d5c5d06c32866142e
hikingupua <hikingupua@gmail.com>
2025-05-09 22:36:49 +0300

    Fix: read DB_PORT from environment

--- hiking/settings.py
+++ hiking/settings.py
@@ -72,9 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'hiking.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -82,7 +79,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': '5432',
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
