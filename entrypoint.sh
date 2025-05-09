9ece5da9a5345f27df56f1799f0a4b79a6d45377
hikingupua <hikingupua@gmail.com>
2025-05-09 22:42:54 +0300

    Force migrate: reset migrations and apply

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+echo "📛 DROPping all existing migrations (локально, якщо треба)"
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
+
+echo "📦 Заново створюємо міграції"
+python manage.py makemigrations
+
 echo "⚙️ Застосовуємо міграції..."
-python manage.py migrate
+python manage.py migrate --noinput
 
 echo "📦 Збираємо статичні файли..."
 python manage.py collectstatic --noinput
