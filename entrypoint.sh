4a140852b7bc0d393b521e9ed7209a09fa44b43a
hikingupua <hikingupua@gmail.com>
2025-06-18 14:53:24 +0300

    Restore normal startup with gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,9 @@
 
 echo "Running database migrations..."
 python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+gunicorn hiking.wsgi:application --bind 0.0.0.0:8000
