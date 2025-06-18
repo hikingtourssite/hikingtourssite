5756a8bc7aa521f6dc1ece2c7a85d759b1adcf93
hikingupua <hikingupua@gmail.com>
2025-06-18 14:25:51 +0300

    Temporarily run migrate only on Render

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
 
-echo "DROPping all existing migrations (локально, якщо треба)"
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
-
-echo "Заново створюємо міграції"
-python manage.py makemigrations
-
-echo "Застосовуємо міграції..."
+echo "Running database migrations..."
 python manage.py migrate --noinput
-
-echo "Збираємо статичні файли..."
-python manage.py collectstatic --noinput
-
-echo "Запускаємо Gunicorn..."
-gunicorn hiking.wsgi:application --bind 0.0.0.0:8000
